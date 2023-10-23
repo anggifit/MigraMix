b328c5825c53a6c6182bbd81c429e5b91ed14540
anggifit <abonaldy@gmail.com>
2023-10-23 17:04:10 +0200

    connecting signUp form with server

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -47,12 +47,12 @@ const SignUpForm = () => {
     if (isValid){
       data.dateOfBirth = selectedDate
       axios 
-        .post('url', data, 
+        .post('/api/signup', data, 
         {headers: { 'Content-Type': 'application/json' }}
         )
         .then(response => {console.log(response.data)})
         .catch(error => {console.log(error.data)})
-        
+
       console.log(data);
     } 
   }
