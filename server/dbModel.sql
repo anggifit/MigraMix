5e94c703257d5b934075e5dd2ce9d4c78bda3708
Damian <Damian.Gamalie@protonmail.com>
2023-10-25 21:43:04 +0200

    Cambios del sql script

--- server/dbModel.sql
+++ server/dbModel.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE migramix
+CREATE DATABASE migramix;
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
@@ -8,18 +8,20 @@ CREATE TABLE users(
     email VARCHAR(120) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     dateOfBirth DATE NOT NULL
-) 
-CREATE TABLE artists(artist_id INT references users(id)) 
-CREATE TABLE events_organiser(organiser_id INT references users(id)) 
+);
+CREATE TABLE artists(artist_id INT UNIQUE references users(id));
+
+CREATE TABLE events_organiser(organiser_id INT UNIQUE references users(id)); 
+
+CREATE TABLE events(
+    id SERIAL PRIMARY KEY,
+    organiser_id INT references events_organiser(organiser_id),
+    start_date DATE NOT NULL,
+    location VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE event_by_artist(
     id SERIAL PRIMARY KEY,
     event_id INT references events(id),
-    event_by_artist_id INT references artist(id)
-) 
-CREATE TABLE events(
-    id SERIAL PRIMARY KEY,
-    organiser_id INT references events_organiser(id),
-    start_date DATE NOT NULL,
-    location VARCHAR(100) NOT NULL
-)
+    event_by_artist_id INT references artists(artist_id)
+);
