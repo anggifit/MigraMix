77b6253f03a5fb6d78fa0f8d3a058e39e9be9df4
Damian <Damian.Gamalie@protonmail.com>
2023-10-25 19:59:58 +0200

    Realizada limpieza de archivos y actualización de .gitignore y README.md
    Se eliminó la repetición de .gitignore y .env, además, se ha creado una plantilla básica para el README.md

--- server/dbModel.sql
+++ server/dbModel.sql
@@ -8,26 +8,15 @@ CREATE TABLE users(
     email VARCHAR(120) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     dateOfBirth DATE NOT NULL
-)
-
-CREATE TABLE artists(
-    artist_id INT references users(id)
-
-    
-)
-
-CREATE TABLE events_organiser(
-    organiser_id INT references users(id)
-    
-)
+) 
+CREATE TABLE artists(artist_id INT references users(id)) 
+CREATE TABLE events_organiser(organiser_id INT references users(id)) 
 
 CREATE TABLE event_by_artist(
     id SERIAL PRIMARY KEY,
     event_id INT references events(id),
     event_by_artist_id INT references artist(id)
-    
-)
-
+) 
 CREATE TABLE events(
     id SERIAL PRIMARY KEY,
     organiser_id INT references events_organiser(id),
