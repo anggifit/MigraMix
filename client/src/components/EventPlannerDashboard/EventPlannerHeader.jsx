230cf8ac0d9b729926c343f6b36a0e7e344ded0c
anggifit <abonaldy@gmail.com>
2023-11-01 11:56:19 +0100

    View profile button fixed

--- client/src/components/EventPlannerDashboard/EventPlannerHeader.jsx
+++ client/src/components/EventPlannerDashboard/EventPlannerHeader.jsx
@@ -7,6 +7,7 @@ const EventPlannerHeader = ({user, profilePhoto, rol}) => {
             src={profilePhoto}
             alt="Avatar user"
             className="w-20 md:w-32 rounded-full mx-auto"
+            style={{maxWidth: "200px"}}
         />
         <h2 className="font-bold text-base text-center text-gray-700 pt-4">
             {user}
