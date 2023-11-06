4dc1845818b1361f665fb295c0e2a29c04fbbcdc
omikeflies <omikeflies@gmail.com>
2023-11-06 12:15:57 +0100

    Fix Warning: validateDOMNesting + Card size event

--- client/src/components/PublicEventsSection/PublicEventsList.jsx
+++ client/src/components/PublicEventsSection/PublicEventsList.jsx
@@ -65,7 +65,7 @@ function PublicEventsList() {
   });
 
  return (
-    <div className="bg-white font-sans p-8">
+    <div className="font-sans p-8">
       <SearchEventsBar onSearch={handleSearch} onFilterChange={handleFilterChange} />
       
       {loading ? (
