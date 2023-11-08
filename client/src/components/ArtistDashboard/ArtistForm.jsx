74aecbf09f97781637c6a856c1680d8448dbd5a5
omikeflies <omikeflies@gmail.com>
2023-11-08 12:52:55 +0100

    input validation Artistform

--- client/src/components/ArtistDashboard/ArtistForm.jsx
+++ client/src/components/ArtistDashboard/ArtistForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import axios from "axios"
+import axios from "axios";
 import UploadProfilePhoto from "../EventPlannerDashboard/UploadProfilePhoto";
 
 const ArtistForm = () => {
@@ -10,37 +10,43 @@ const ArtistForm = () => {
     formState: { isValid, errors },
   } = useForm({
     defaultValues: {
-      artistbio: ''
-    }
+      artistbio: "",
+    },
   });
+  console.log(errors);
 
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
+  const socialMediaRegex =
+    /^((https?|ftp|smtp):\/\/)?(www\.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
 
-  const [eventProfilePictureURL, setEventProfilePictureURL] = useState(null)
+  const [eventProfilePictureURL, setEventProfilePictureURL] = useState(null);
   
+
   const onImageUpload = (url) => {
-    setEventProfilePictureURL(url)
-/*     onUpdateProfilePhoto(url)
- */  }
-  
+    setEventProfilePictureURL(url);
+    /*     onUpdateProfilePhoto(url)
+     */
+  };
+
   const onSubmit = (data) => {
-    console.log(data)
-    if (isValid){
+    // console.log(data);
+    if (isValid) {
       data.eventProfilePicture = eventProfilePictureURL;
       axios
-      .put(`/api/artists`, data, 
-      {headers: {
-          Authorization: `Bearer ${token}`
-      }})
-      .then(response => {
-          console.log(response.data)
-      })
-      .catch(error => {console.log(error.response.data)})
-  }
-  }
-  
-  console.log(errors);
-  
+        .put(`/api/artists`, data, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+    }
+  };
+
   return (
     <div className="w-full relative  shadow-2xl rounded overflow-hidden">
       <div className="top h-full w-full bg-blue-600 overflow-hidden relative">
@@ -64,19 +70,19 @@ const ArtistForm = () => {
         <div className="col-span-12 w-full px-3 py-6 justify-center flex space-x-4 border-b border-solid md:space-x-0 md:space-y-4 md:flex-col md:col-span-2 md:justify-start ">
           <a
             href="#"
-            className="text-sm p-2 bg-indigo-900 text-white text-center rounded font-bold"
+            className="text-sm p-2 bg-indigo-900 shadow-xl text-white text-center rounded font-bold"
           >
             Edit Profile 🖼
           </a>
           <a
             href="#"
-            className="text-sm p-2 bg-indigo-200 text-center rounded font-semibold hover:bg-indigo-700 hover:text-gray-200"
+            className="text-sm p-2 bg-white text-center rounded shadow-xl font-semibold hover:bg-indigo-700 hover:text-gray-200 md:transition-all"
           >
             Another Information
           </a>
           <a
             href="#"
-            className="text-sm p-2 bg-indigo-200 text-center rounded font-semibold hover:bg-indigo-700 hover:text-gray-200"
+            className="text-sm p-2 bg-white text-center rounded shadow-xl font-semibold hover:bg-indigo-700 hover:text-gray-200 md:transition-all "
           >
             Log-out 🔒
           </a>
@@ -88,10 +94,7 @@ const ArtistForm = () => {
               className="flex flex-col space-y-2 "
               action="#"
             >
-              <div>
-                {/* <h3 className="text-1xl ">Basic Information</h3> */}
-                <hr />
-              </div>
+              {/* <h3 className="text-1xl ">Basic Information</h3> */}
 
               {/* <div className="form-item">
                 <label className="text-lg "></label>
@@ -150,68 +153,108 @@ const ArtistForm = () => {
                 </div>
               </div> */}
 
-              <div>
-                {/* <h3 className="text-2xl ">More About Me</h3> */}
-                <hr />
-              </div>
+              <div>{/* <h3 className="text-2xl ">More About Me</h3> */}</div>
 
               <div className="form-item w-full">
-                <label className="text-sm "></label>
+                {/* <label className="text-sm "></label> */}
                 <textarea
                   cols="10"
                   rows="5"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
+                  className="w-full appearance-none text-black text-opacity-50 rounded shadow-xl py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
                   placeholder="Bio"
+                  maxLength={350}
+                  minLength={50}
                   id="artistbio"
-                  {...register("artistbio", { maxLength: 350 })}
+                  {...register("artistbio", { maxLength: 350, minLength: 50 })}
                 />
+                {errors.artistbio && (
+                  <p className="text-red-500 text-sm">
+                    {errors.artistbio.message}
+                  </p>
+                )}
               </div>
-              <div>
-                {/* <h3 className="text-2xl">My Social Media</h3> */}
-                <hr />
-                <hr />
-              </div>
+              <div>{/* <h3 className="text-2xl">My Social Media</h3> */}</div>
               <div className="form-item">
                 <label className="text-xl "></label>
                 <input
                   type="text"
                   placeholder="Instagram"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 "
-                  {...register("instagram", {})}
+                  className="w-full appearance-none text-black text-opacity-50 rounded shadow-xl py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 "
+                  {...register("instagram", {
+                    pattern: {
+                      value: socialMediaRegex,
+                      message: "Not a valid Link",
+                    },
+                  })}
                 />
+                {errors.instagram && (
+                  <p className="text-red-500 text-sm">
+                    {errors.instagram.message}
+                  </p>
+                )}
               </div>
               <div className="form-item">
-                <label className="text-xl "></label>
                 <input
                   type="text"
                   placeholder="Facebook"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
-                  {...register("facebook", {})}
+                  className="w-full appearance-none text-black text-opacity-50 rounded shadow-xl py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
+                  {...register("facebook", {
+                    pattern: {
+                      value: socialMediaRegex,
+                      message: "Not a valid Link",
+                    },
+                  })}
                 />
+                {errors.facebook && (
+                  <p className="text-red-500 text-sm">
+                    {errors.facebook.message}
+                  </p>
+                )}
               </div>
               <div className="form-item">
                 <label className="text-xl "></label>
                 <input
                   type="text"
                   placeholder="Twitter"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200  "
-                  {...register("twitter", {})}
+                  className="w-full appearance-none text-black text-opacity-50 rounded shadow-xl py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200  "
+                  {...register("twitter", {
+                    pattern: {
+                      value: socialMediaRegex,
+                      message: "Not a valid Link",
+                    },
+                  })}
                 />
+                {errors.twitter && (
+                  <p className="text-red-500 text-sm">
+                    {errors.twitter.message}
+                  </p>
+                )}
               </div>
               <div className="form-item">
                 <label className="text-xl "></label>
                 <input
                   type="text"
                   placeholder="Soundcloud"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200  "
-                  {...register("soundcloud", {})}
+                  className="w-full appearance-none text-black text-opacity-50 rounded shadow-xl py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200  "
+                  {...register("soundcloud", {
+                    pattern: {
+                      value: socialMediaRegex,
+                      message: "Not a valid Link",
+                    },
+                  })}
                 />
+                {errors.soundcloud && (
+                  <p className="text-red-500 text-sm">
+                    {errors.soundcloud.message}
+                  </p>
+                )}
               </div>
               <div>
-                <UploadProfilePhoto onImageUpload={onImageUpload}/>
+                <UploadProfilePhoto onImageUpload={onImageUpload} />
               </div>
-              <button className="flex flex-auto items-left max-w-2xl justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-gray-900 rounded-sm group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-600">
-                <input type="submit" placeholder="send" />{} 📤 
+              <button className="flex flex-auto items-left max-w-1xl justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-gray-900 rounded-sm group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-600">
+                <input type="submit" placeholder="send" />
+                {} 📤
               </button>
             </form>
           </div>
