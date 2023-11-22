652905e812a901b4b39d42a64dcfddee8e2f252b
omikeflies <omikeflies@gmail.com>
2023-11-22 16:09:37 +0100

    set responsive parameters for the filters

--- client/src/components/PublicEventsSection/Filters.jsx/FilterByEvent.jsx
+++ client/src/components/PublicEventsSection/Filters.jsx/FilterByEvent.jsx
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
-import { TextField, InputAdornment } from "@mui/material";
+import { TextField, InputAdornment, useTheme } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const FilterByEvent = ({ onSearch }) => {
+  const theme = useTheme();
+
   const handleSearch = (event) => {
     onSearch(event.target.value);
   };
@@ -13,7 +15,15 @@ const FilterByEvent = ({ onSearch }) => {
         id="titleFilter"
         label="By event"
         onChange={handleSearch}
-        sx={{ width: 600 }}
+        sx={{
+          width: "100%", // Set width to 100% for small screens
+          [theme.breakpoints.up("sm")]: {
+            width: 300, // Adjust width for screens wider than or equal to sm
+          },
+          [theme.breakpoints.up("md")]: {
+            width: 400, // Adjust width for screens wider than or equal to md
+          },
+        }}
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
