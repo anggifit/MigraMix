8932f1f37869346d020d81471437a1fd5fe05398
anggifit <abonaldy@gmail.com>
2024-03-06 17:56:34 +0100

    updating

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-import server from './server.js';
+import server from './src/server.js'
+
 const PORT = 4000;
 
 server.listen(PORT, () => console.log(`server listen in port ${PORT}`));
--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-
-dotenv.config();
-
 import morgan from "morgan";
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
@@ -10,12 +7,14 @@ import organizerRoutes from "./routes/organizer.routes.js";
 import artistRoutes from "./routes/artist.routes.js";
 import eventsRoutes from "./routes/event.routes.js";
 
+dotenv.config();
+
 const server = express();
 server.use(morgan("dev"));
 
 server.use(
   cors({
-    origin: "http://localhost:5174", //verificar la direccion localhost que se indica al levantar el cliente 
+    origin: "https://migra-1sx8vnhmf-anggifits-projects.vercel.app", //verificar la direccion localhost que se indica al levantar el cliente 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
